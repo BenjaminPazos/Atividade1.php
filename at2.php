c8c17414bcd9b34ac264db1b1790590d9b88d8c9
BenjaminPazos <20251FOZ0020031@estudantes.ifpr.edu.br>
2025-10-31 15:22:13 -0300

    Create at2.php

---
+++ at2.php
@@ -0,0 +1,28 @@
+<?php
+
+$chute = 0;
+$tentativas = 1;
+
+$intervalo1 = (int) readline("Digite um número menor para o intervalo: ");
+$intervalo2 = (int) readline("Digite um número maior para o intervalo: ");
+
+$sorteado = rand($intervalo1, $intervalo2);
+
+while ($chute != $sorteado) {
+
+    $chute = (int) readline("Digite um número: ");
+
+    if ($chute == $sorteado) {
+        print("Você venceu!!! \n");
+        print("Número de tentativas:$tentativas \n");
+    } else {
+
+        ++$tentativas;
+
+        if ($chute > $sorteado) {
+            print("Valor muito alto \n");
+        } else {
+            print("Valor muito baixo \n");
+        }
+    }
+}
